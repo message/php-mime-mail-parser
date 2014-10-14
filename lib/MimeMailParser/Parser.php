fc6dfe54d68d03986ae48a4449a9774b2eff3f81
Dmitry Stanulevich <dmitry@textmagic.biz>
2014-10-14 11:16:28 +0700

    getMessageBody does not handle Content-disposition inline fix

--- lib/MimeMailParser/Parser.php
+++ lib/MimeMailParser/Parser.php
@@ -207,7 +207,7 @@ class Parser
 		);
 		if (in_array($type, array_keys($mime_types))) {
 			foreach ($this->parts as $part) {
-				if ($this->getPartContentType($part) == $mime_types[$type] && !$this->getPartContentDisposition($part)) {
+				if ($this->getPartContentType($part) == $mime_types[$type] && (!$this->getPartContentDisposition($part) || $this->getPartContentDisposition($part) === 'inline')) {
 					$headers = $this->getPartHeaders($part);
 					$body = $this->decode($this->getPartBody($part), array_key_exists('content-transfer-encoding', $headers) ? $headers['content-transfer-encoding'] : '');
 				}
